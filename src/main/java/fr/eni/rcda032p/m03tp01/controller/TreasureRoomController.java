e3684db9e4432bd7bcd90ff583c7421b2c962e11
thomasdeloneni <149668246+thomasdeloneni@users.noreply.github.com>
2024-01-16 16:37:45 +0100

    modification decorateur

--- src/main/java/fr/eni/rcda032p/m03tp01/controller/TreasureRoomController.java
+++ src/main/java/fr/eni/rcda032p/m03tp01/controller/TreasureRoomController.java
@@ -3,9 +3,10 @@ package fr.eni.rcda032p.m03tp01.controller;
 
 import fr.eni.rcda032p.m03tp01.bll.TreasureService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 
-@Controller
+@Component
 public class TreasureRoomController {
     private TreasureService tresorService;
 
