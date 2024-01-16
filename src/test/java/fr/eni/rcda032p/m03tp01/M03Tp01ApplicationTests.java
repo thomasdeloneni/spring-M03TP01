6e2633cf1401c53c1a5229ef5dade401be5b75c1
thomasdeloneni <149668246+thomasdeloneni@users.noreply.github.com>
2024-01-16 14:53:26 +0100

    first commit

---
+++ src/test/java/fr/eni/rcda032p/m03tp01/M03Tp01ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.rcda032p.m03tp01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M03Tp01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
